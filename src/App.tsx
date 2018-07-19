37540d0e6c97f4c56f8805c95935e3c92533d1f5
Misha <misha@gomisha.com>
2018-07-19 17:56:34 -0400

    added skeleton Navigation component
    removed unneeded starter files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
+import "tachyons";
 import './App.css';
+import Navigation from "./components/navigation";
 
-import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Navigation/>
       </div>
     );
   }
