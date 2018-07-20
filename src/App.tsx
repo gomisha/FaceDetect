5ce1b731d6099390596c4c32c39d479c97151b0b
Misha <misha@gomisha.com>
2018-07-19 22:17:36 -0400

    added Rank component skeleton
    added react-particles-js component for cool background effects

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,30 @@ import "tachyons";
 import './App.css';
 import Logo from "./components/logo";
 import Navigation from "./components/navigation";
+import Rank from "./components/rank";
 import ImageLinkForm from "./components/image-link-form";
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+    particles: {
+        number: {
+            density: {
+                enable: true,
+                value_area: 800
+            },
+            value: 30
+        }
+    }
+};
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <Navigation/>
+        <Particles params={particlesOptions} className="particles"/>
         <Logo/>
+        <Rank/>
         <ImageLinkForm/>
       </div>
     );
---
+++ src/components/rank/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="white f3">
+                {"Misha, your current rank is..."}
+            </div>
+            <div className="white f1">
+                {"#2"}
+            </div>
+        </div>
+    );
+};
+
+export default Rank;
